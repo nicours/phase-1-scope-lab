3e7877c3cd07dc04d3cfad4630db6eb7966e1560
Nicodemus Kiplangat <nicodemusk84@gmail.com>
2024-07-02 09:39:28 -0400

    Fix scope issues and define required functions and variables

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+// Function to uppercase the customerName variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+}
+
+// Declare leastFavoriteCustomer in global scope with const
+const leastFavoriteCustomer = 'someone';
+
+// Function to try changing leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+    // This should cause an error since leastFavoriteCustomer is a const
+    leastFavoriteCustomer = 'new value';
+}
+
+module.exports = {
+    customerName,
+    upperCaseCustomerName,
+    setBestCustomer,
+    overwriteBestCustomer,
+    changeLeastFavoriteCustomer
+};
+
